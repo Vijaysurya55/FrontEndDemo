84ab7f7cffe6f566a0a98eea4e842af5c8fd00f0
Vijaysurya55 <vijaysurya112003@gmail.com>
2025-01-06 15:14:47 +0530

    vercel file

--- src/createUser.jsx
+++ src/createUser.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
-const navigate=useNavigate
+const navigate=useNavigate();
 
 const CreateUser = () => {
   const [name,setName]=useState("");
